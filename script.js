cd4493cbb31ed1efc4a9278a395630500d6ad9fe
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-02-26 20:21:37 -0600

    Added a folder called templates and moved html files to that folder. Also added an htmlRenderer.js file from class repo. Finally, added some variable given to us in app.js that I didn't have previous and will need for code to work.

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ const Intern = require("./lib/intern.js");
 
 const inquirer = require("inquirer");
 
+const path = require("path");
+
 // const jest = require("jest");
 
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+​
+const render = require("./lib/htmlRenderer");
 
 
 const managerQuestions = [
